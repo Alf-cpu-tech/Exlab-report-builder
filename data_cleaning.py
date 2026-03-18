d939d57b213e450aa5b3c246e1aabe3944949eae
Alf-cpu-tech <alfieridgway31@gmail.com>
2026-03-18 11:13:47 +0000

    Update data_cleaning.py
    trying to use rolling window to get more accurate error fixes

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,14 +7,20 @@ dirtyData = pd.read_excel("Copy of CPET_DM01__Max Breath by Breath_class.xlsx")
 #the following code was taken from https://www.geeksforgeeks.org/data-analysis/how-to-automate-data-cleaning-in-python/
 #im going to try and adapt it for our purposes
 def outlier_handling(df, column_with_outliers):
-    q1 = df[column_with_outliers].quantile(0.25)
-    q3 = df[column_with_outliers].quantile(0.75)
+    q1 = df[column_with_outliers].rolling(window=4).quantile(0.25)
+    print(q1)
+    q3 = df[column_with_outliers].rolling(window=4).quantile(0.75)
+    print(q3)
     iqr = q3 - q1
     # remove outliers
-    df = df[(df[column_with_outliers] > (q1 - 1.5 * iqr)) 
-            & (df[column_with_outliers] < (q3 + 1.5 * iqr))] 
+    df = df[(df[column_with_outliers] > (q1 - 1.1 * iqr)) 
+            & (df[column_with_outliers] < (q3 + 1.1 * iqr))] 
     return df
 
-cleanData = outlier_handling(dirtyData, "VT")
+
 pd.set_option("display.max_rows", None)
-print(df)
+pd.set_option("display.max_columns", 6)
+#calling the function and passing the
+cleanData = outlier_handling(dirtyData, "V'O2")
+
+print(cleanData)
